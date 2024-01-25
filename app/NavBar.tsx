58aeb137f5d42f1ff731207fbad929677c92de45
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2024-01-25 15:20:06 +0100

    connected_with_mysql_and_design_started

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+import { nanoid } from "nanoid";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { id: nanoid(), label: "Dashboard", href: "/" },
+    { id: nanoid(), label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link
+              className={classNames({
+                "text-zic-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
